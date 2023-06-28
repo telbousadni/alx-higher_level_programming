8cd035daaffca476f3b958156986dfd6b192b555
telbousadni <taha.elbousadni@gmail.com>
2023-06-27 18:00:50 -0700

    D

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
 
@@ -7,7 +8,6 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new Square.
-
         Args:
             size (int): The size of the new square.
         """
@@ -16,4 +16,3 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
-
