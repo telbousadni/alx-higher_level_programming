1b07af0f80d21a1c21e5acaef276fb9a205187cf
telbousadni <taha.elbousadni@gmail.com>
2023-08-16 14:39:14 -0700

    fixed cmds

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- List all privileges for users 'user_0d_1' and 'user_0d_2'
+-- List all privileges for users 'user_0d_1'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- List all privileges for users 'user_0d_2'
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
