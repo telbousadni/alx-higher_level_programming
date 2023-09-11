7ee8514f2424a76b09e586c6dee864e4d96b779a
telbousadni <taha.elbousadni@gmail.com>
2023-09-11 14:35:55 -0700

    3 lines fix

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,4 +2,6 @@
 const a = 'C is fun';
 const b = 'Python is cool';
 const c = 'JavaScript is amazing';
-console.log(a); console.log(b); console.log(c);
+console.log(a);
+console.log(b);
+console.log(c);
